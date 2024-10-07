1bc53f2b930682eb2ecc5f7afd53f0254f4adaa7
abuduj3na <meric.ucar@icloud.com>
2024-10-07 20:40:51 +0200

    KLEUR-03

--- Ucar_Meric_KleurenPaneel/Ucar_Meric_KleurenPaneel/MainWindow.xaml.cs
+++ Ucar_Meric_KleurenPaneel/Ucar_Meric_KleurenPaneel/MainWindow.xaml.cs
@@ -16,9 +16,40 @@ namespace Ucar_Meric_KleurenPaneel
     /// </summary>
     public partial class MainWindow : Window
     {
+        Grid Hoofdscherm;
+        string Rood = "#FF0000";
+        string Geel = "#FFFF00";
+        string Blauw = "#0000FF";
+        string Groen = "#008000";
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Hoofdscherm = hoofdscherm;
+        }
+
+        private void Image_MouseEnter_Rood(object sender, MouseEventArgs e)
+        {
+            // https://stackoverflow.com/questions/10062376/creating-solidcolorbrush-from-hex-color-value
+            Hoofdscherm.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(Rood);
+        }
+        private void Image_MouseEnter_Geel(object sender, MouseEventArgs e)
+        {
+            Hoofdscherm.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(Geel);
+        }
+        private void Image_MouseEnter_Blauw(object sender, MouseEventArgs e)
+        {
+            Hoofdscherm.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(Blauw);
+        }
+        private void Image_MouseEnter_Groen(object sender, MouseEventArgs e)
+        {
+            Hoofdscherm.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(Groen);
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Hoofdscherm.Background = Brushes.LightGray;
         }
     }
 }
